c5a18b1e1e6e53ed2f604877a64602707d7ec86f
Bibek Gurung <bibek.gurung95@gmail.com>
2022-06-10 16:38:27 +0545

    created weather base page

---
+++ src/app/weather/weather.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { WeatherPageComponent } from './weather-page/weather-page.component';
+
+@NgModule({
+  declarations: [WeatherPageComponent],
+  imports: [CommonModule],
+})
+export class WeatherModule {}

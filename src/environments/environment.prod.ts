eceb13fd760c58c352c0462e8280779abdc08d0d
Bibek Gurung <bibek.gurung95@gmail.com>
2022-06-12 17:26:11 +0545

    updated env data

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  weatherAPIBaseURL: 'http://dataservice.accuweather.com/',
+  weatherAPIKey: 'Al3IHDAr6CSBAUc5LHQdAlGlsAqqAUrN'
 };
